9232926380f54638626af1a043ed8254934af123
arupong <35568965+arupong@users.noreply.github.com>
2018-01-31 16:15:25 +0700

    Update settings.py

--- RealMooPort/RealMooPort/settings.py
+++ RealMooPort/RealMooPort/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w(e6v4de-$mkp#ekt4nk_-_n17t-y0$qd#df+nktw192t9e+rl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://arupongmoo.pythonanywhere.com/']
 
 
 # Application definition
